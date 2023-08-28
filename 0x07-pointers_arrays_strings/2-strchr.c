de33ed96d10bbc5bf8741a66cb9c7aaea35bf7d4
idrisaloma1 <sofiyyahidris23@gmail.com>
2023-08-28 06:16:10 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,13 @@
  * Return: pointer to first occurence of character c
  * or NULL if character is not found.
  */
-
 char *_strchr(char *s, char c)
 {
 while (*s != '\0')
 {
 if (*s == c)
-return (s);else if (*(s + 1) == c)
+return (s);
+else if (*(s + 1) == c)
 return (s + 1);
 s++;
 }
